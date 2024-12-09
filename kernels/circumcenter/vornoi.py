b1287a5135d871c9c41e5acaa7ac3dfb5e829514
Christopher Stange <cjstange@andrew.cmu.edu>
2024-12-09 04:17:05 -0500

    vornoi.py done

--- kernels/circumcenter/vornoi.py
+++ kernels/circumcenter/vornoi.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 NUM_POINTS = 100
-MAX_VAL = 100000
+MAX_VAL = 10000
 
 class Vertex:
     def __init__(self, x, y):
@@ -34,7 +34,14 @@ class Triangle:
             self.v0 = v0
             self.v1 = v2
             self.v2 = v1
-        self.circumcircle = None
+        self.e0 = Edge(self.v0, self.v1)
+        self.e1 = Edge(self.v1, self.v2)
+        self.e2 = Edge(self.v2, self.v0)
+        self.neighbors = []
+        self.circumcenter = None
+        self.vornoi_e0 = None
+        self.vornoi_e1 = None
+        self.vornoi_e2 = None
 
     def inCircumcircle(self, vertex):
         matrix = np.array([[self.v0.x, self.v0.y, self.v0.x**2 + self.v0.y**2, 1],
@@ -49,6 +56,36 @@ class Triangle:
                            [v1.x, v1.y, 1],
                            [v2.x, v2.y, 1]])
         return np.linalg.det(matrix) > 0
+
+    def isNeighbor(self, other):
+        return (self.e0 == other.e0 or self.e0 == other.e1 or self.e0 == other.e2 or
+                self.e1 == other.e0 or self.e1 == other.e1 or self.e1 == other.e2 or
+                self.e2 == other.e0 or self.e2 == other.e1 or self.e2 == other.e2)
+
+    def calculateCircumcenter(self):
+        D = 2 * ((self.v0.x * (self.v1.y - self.v2.y)) +
+                 (self.v1.x * (self.v2.y - self.v0.y)) +
+                 (self.v2.x * (self.v0.y - self.v1.y)))
+
+        assert D != 0.0, "Points are colinear"
+
+        Ux = (((self.v0.x**2 + self.v0.y**2) * (self.v1.y - self.v2.y)) + 
+              ((self.v1.x**2 + self.v1.y**2) * (self.v2.y - self.v0.y)) +
+              ((self.v2.x**2 + self.v2.y**2) * (self.v0.y - self.v1.y))) / D
+        
+        Uy = (((self.v0.x**2 + self.v0.y**2) * (self.v2.x - self.v1.x)) +
+              ((self.v1.x**2 + self.v1.y**2) * (self.v0.x - self.v2.x)) +
+              ((self.v2.x**2 + self.v2.y**2) * (self.v1.x - self.v0.x))) / D
+
+        # print(self.v0.x, self.v0.y, self.v1.x, self.v1.y, self.v2.x, self.v2.y, Ux, Uy, D,
+        #       (((self.v0.x**2 + self.v0.y**2) * (self.v1.y - self.v2.y)) + 
+        #       ((self.v1.x**2 + self.v1.y**2) * (self.v2.y - self.v0.y)) +
+        #       ((self.v2.x**2 + self.v2.y**2) * (self.v0.y - self.v1.y))),
+        #       (((self.v0.x**2 + self.v0.y**2) * (self.v2.x - self.v1.x)) +
+        #       ((self.v1.x**2 + self.v1.y**2) * (self.v0.x - self.v2.x)) +
+        #       ((self.v2.x**2 + self.v2.y**2) * (self.v1.x - self.v0.x))))
+
+        self.circumcenter = Vertex(Ux, Uy)
 
 
 def superTriangle(points_list):
@@ -126,10 +163,76 @@ def bowyerWatson(points_list):
         if not (triangle.v0 == st.v0 or triangle.v0 == st.v1 or triangle.v0 == st.v2 or
                 triangle.v1 == st.v0 or triangle.v1 == st.v1 or triangle.v1 == st.v2 or
                 triangle.v2 == st.v0 or triangle.v1 == st.v1 or triangle.v2 == st.v2):
-            print(triangle.v2.y)
             filtered_triangles.append(triangle)
 
     return filtered_triangles
+
+
+def print_delaunay(triangles, ax):
+    for triangle in triangles:
+        e0 = Edge(triangle.v0, triangle.v1)
+        e1 = Edge(triangle.v1, triangle.v2)
+        e2 = Edge(triangle.v2, triangle.v0)
+
+        xdata = [e0.v0.x, e0.v1.x]
+        ydata = [e0.v0.y, e0.v1.y]
+        ax.plot(xdata, ydata, color="black")
+        ax.scatter(xdata, ydata, color="black")
+
+        xdata = [e1.v0.x, e1.v1.x]
+        ydata = [e1.v0.y, e1.v1.y]
+        ax.plot(xdata, ydata, color="black")
+        ax.scatter(xdata, ydata, color="black")
+
+        xdata = [e2.v0.x, e2.v1.x]
+        ydata = [e2.v0.y, e2.v1.y]
+        ax.plot(xdata, ydata, color="black")
+        ax.scatter(xdata, ydata, color="black")
+
+
+def vornoi(triangles):
+    # Calculate circumcenters
+    for triangle in triangles:
+        triangle.calculateCircumcenter()
+    
+    # Get triangle neighbors
+    for i in range(0, len(triangles)):
+        for j in range(0, len(triangles)):
+            if i != j and triangles[i].isNeighbor(triangles[j]):
+                triangles[i].neighbors.append(triangles[j])
+
+    # Get vornoi diagram
+    for triangle in triangles:
+        num_neighbors = len(triangle.neighbors)
+        assert num_neighbors <= 3, "More than 3 neighbors for a triangle"
+        if (1 <= len(triangle.neighbors)):
+            triangle.vornoi_e0 = Edge(triangle.circumcenter, triangle.neighbors[0].circumcenter)
+        if (2 <= len(triangle.neighbors)):
+            triangle.vornoi_e1 = Edge(triangle.circumcenter, triangle.neighbors[1].circumcenter)
+        if (3 <= len(triangle.neighbors)):
+            triangle.vornoi_e2 = Edge(triangle.circumcenter, triangle.neighbors[2].circumcenter)
+
+
+def print_vornoi(triangles, ax):
+    #FIXME: THIS IS INEFFICEINT, there are duplicates of the vornoi edges    
+    for triangle in triangles:
+        if triangle.vornoi_e0:
+            xdata = [triangle.vornoi_e0.v0.x, triangle.vornoi_e0.v1.x]
+            ydata = [triangle.vornoi_e0.v0.y, triangle.vornoi_e0.v1.y]
+            ax.plot(xdata, ydata, color="red")
+            ax.scatter(xdata, ydata, color="red")
+
+        if triangle.vornoi_e1:
+            xdata = [triangle.vornoi_e1.v0.x, triangle.vornoi_e1.v1.x]
+            ydata = [triangle.vornoi_e1.v0.y, triangle.vornoi_e1.v1.y]
+            ax.plot(xdata, ydata, color="red")
+            ax.scatter(xdata, ydata, color="red")
+
+        if triangle.vornoi_e2:
+            xdata = [triangle.vornoi_e2.v0.x, triangle.vornoi_e2.v1.x]
+            ydata = [triangle.vornoi_e2.v0.y, triangle.vornoi_e2.v1.y]
+            ax.plot(xdata, ydata, color="red")
+            ax.scatter(xdata, ydata, color="red")
 
 
 if __name__ == '__main__':
@@ -140,25 +243,12 @@ if __name__ == '__main__':
 
     fig = plt.figure()
     ax = fig.add_subplot()
-    
-    for triangle in triangles:
-        e0 = Edge(triangle.v0, triangle.v1)
-        e1 = Edge(triangle.v1, triangle.v2)
-        e2 = Edge(triangle.v2, triangle.v0)
 
-        xdata = [e0.v0.x, e0.v1.x]
-        ydata = [e0.v0.y, e0.v1.y]
-        ax.plot(xdata, ydata)
-        ax.scatter(xdata, ydata)
+    print_delaunay(triangles, ax)
 
-        xdata = [e1.v0.x, e1.v1.x]
-        ydata = [e1.v0.y, e1.v1.y]
-        ax.plot(xdata, ydata)
-        ax.scatter(xdata, ydata)
+    vornoi(triangles)
 
-        xdata = [e2.v0.x, e2.v1.x]
-        ydata = [e2.v0.y, e2.v1.y]
-        ax.plot(xdata, ydata)
-        ax.scatter(xdata, ydata)
+    print_vornoi(triangles, ax)
 
     plt.show()
+
