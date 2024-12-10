5d52d69e8d57b50c970536f250e720f7ff271588
Christopher Stange <cjstange@andrew.cmu.edu>
2024-12-09 20:38:08 -0500

    openmp

--- kernels/circumcenter/kernel_driver.c
+++ kernels/circumcenter/kernel_driver.c
@@ -1,5 +1,6 @@
 #include "kernel.h"
 #include <math.h>
+#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -12,17 +13,17 @@
 
 #define RUNS 100000
 
-#define KERNEL_ITERS_PER_THREAD 1
+#define KERNEL_ITERS_PER_THREAD 20
 
-#define NUM_THREADS 8
+#define NUM_THREADS 2
 
 #define KERNEL_ITERS (KERNEL_ITERS_PER_THREAD * NUM_THREADS)
 
 // kernel0 + kernel1
 // SIMD_SIZE * NUM_OPS * NUM_SIMD_IN_KERNEL
 #define OPS ((SIMD_SIZE * 30 * 6) + (SIMD_SIZE * 4 * 6))
-// kernel0 + kernel1_reciprocal
-// #define OPS ((2 * SIMD_SIZE * 30 * 6) + (SIMD_SIZE * 4 * 12))
+
+#define CACHELINE 64
 
 static __inline__ unsigned long long rdtsc(void) {
   unsigned hi, lo;
@@ -39,7 +40,8 @@ int main(void) {
 
   posix_memalign((void **)&data, ALIGNMENT,
                  KERNEL_ITERS * sizeof(kernel_data_t));
-  posix_memalign((void **)&buffer, ALIGNMENT, sizeof(kernel_buffer_t));
+  posix_memalign((void **)&buffer, ALIGNMENT,
+                 NUM_THREADS * sizeof(kernel_buffer_t));
 
   // Initialize data
   for (int i = 0; i < KERNEL_ITERS; i++) {
@@ -64,44 +66,42 @@ int main(void) {
   }
 
   // Initialize buffer
-  for (int i = 0; i < NUM_SIMD_IN_KERNEL; i++) {
-    for (int j = 0; j < SIMD_SIZE; j++) {
-      buffer->buffer[i].partUx[j] = 0.0;
-      buffer->buffer[i].partUy[j] = 0.0;
-      buffer->buffer[i].partD[j] = 0.0;
+  for (int i = 0; i < NUM_THREADS; i++) {
+    for (int j = 0; j < NUM_SIMD_IN_KERNEL; j++) {
+      for (int k = 0; k < SIMD_SIZE; k++) {
+        buffer[i].buffer[j].partUx[k] = 0.0;
+        buffer[i].buffer[j].partUy[k] = 0.0;
+        buffer[i].buffer[j].partD[k] = 0.0;
+      }
     }
   }
 
-  unsigned long long sum, t0, t1;
-
-  sum = 0;
+  unsigned long long sum[NUM_THREADS * CACHELINE], t0[NUM_THREADS * CACHELINE],
+      t1[NUM_THREADS * CACHELINE];
+  for (int i = 0; i < NUM_THREADS; i++) {
+    sum[i * CACHELINE] = 0;
+    t0[i * CACHELINE] = 0;
+    t1[i * CACHELINE] = 0;
+  }
 
   // Test kernels
   for (int i = 0; i < RUNS; i++) {
-    // for (int j = 0; j < KERNEL_ITERS; j = j + 2) {
-    //   t0 = rdtsc();
-    //   kernel0(&(data[j]), buffer);
-    //   kernel0(&(data[j + 1]), &buffer[NUM_SIMD_IN_KERNEL]);
-    //   kernel1_reciprocal(&(data[j]), buffer);
-    //   // baseline(&(data[j]));
-    //   t1 = rdtsc();
-    //   sum += (t1 - t0);
-    // }
-    for (int j = 0; j < KERNEL_ITERS; j++) {
-      t0 = rdtsc();
-      kernel0(&(data[j]), buffer);
-      kernel1(&(data[j]), buffer);
-      // baseline(&(data[j]));
-      t1 = rdtsc();
-      sum += (t1 - t0);
+#pragma omp parallel for num_threads(NUM_THREADS)
+    for (int j = 0; j < NUM_THREADS; j++) {
+      for (int k = 0; k < KERNEL_ITERS_PER_THREAD; k++) {
+        t0[j * CACHELINE] = rdtsc();
+        kernel0(&(data[j * KERNEL_ITERS_PER_THREAD + k]), &buffer[j]);
+        kernel1(&(data[j * KERNEL_ITERS_PER_THREAD + k]), &buffer[j]);
+        // baseline(&(data[j * KERNEL_ITERS_PER_THREAD + k]));
+        t1[j * CACHELINE] = rdtsc();
+        sum[j * CACHELINE] += (t1[j * CACHELINE] - t0[j * CACHELINE]);
+      }
     }
   }
 
   // printf("%d\n", OPS * KERNEL_ITERS);
-  printf(" %lf\n", (OPS * KERNEL_ITERS) /
-                       ((double)(sum / RUNS) * (MAX_FREQ / BASE_FREQ)));
-  // printf(" %lf\n", (OPS * (KERNEL_ITERS / 2)) /
-  //                      ((double)(sum / RUNS) * (MAX_FREQ / BASE_FREQ)));
+  printf(" %lf\n", (OPS * KERNEL_ITERS_PER_THREAD) /
+                       ((double)(sum[0] / RUNS) * (MAX_FREQ / BASE_FREQ)));
 
   printf("First kernel: %f %f\n", data[0].data[0].Ux[0], data[0].data[0].Uy[0]);
   // printf("Second kernel: %f %f\n", data[1].data[0].Ux[0],
